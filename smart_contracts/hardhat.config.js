bae625d7e15c3f3235064c7b2e5058fa19951350
Tanu Gupta <guptatanukhandelwal.96@gmail.com>
2022-08-07 14:42:38 +0530

    Update hardhat.config.js

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: '0.8.9',
   networks: {
     goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/kIVbNKWhoMEVMl_wnL_P2LSdhQjNuntm',
-      accounts: ['2de072b236cd4ae4b3f14aeb75efd9ff4c73dd1c8884472da184bf20a4568614'],
+      url: 'https://eth-goerli.g.alchemy.com/v2/*************',
+      accounts: ['000*******123************456*********789*******000'],
     },
   },
 };
